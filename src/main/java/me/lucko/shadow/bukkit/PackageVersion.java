fc60b57e5c535b6566e768c15e98fcff2ddf37ab
Nicholas Badger <awildferus@gmail.com>
2019-12-17 09:40:03 -0500

    Add 1.15_R1 package version (#2)

--- src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
+++ src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
@@ -52,7 +52,8 @@ public enum PackageVersion {
     v1_12_R1,
     v1_13_R1,
     v1_13_R2,
-    v1_14_R1;
+    v1_14_R1,
+    v1_15_R1;
 
     /**
      * The nms prefix (without the version component)
