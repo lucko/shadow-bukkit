a30768f4606dfe7304c068f9f223ae74e859e1d1
Luck <git@lucko.me>
2020-06-26 19:44:47 +0100

    Add package version for 1_16_R1

--- src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
+++ src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
@@ -53,7 +53,8 @@ public enum PackageVersion {
     v1_13_R1,
     v1_13_R2,
     v1_14_R1,
-    v1_15_R1;
+    v1_15_R1,
+    v1_16_R1;
 
     /**
      * The nms prefix (without the version component)
