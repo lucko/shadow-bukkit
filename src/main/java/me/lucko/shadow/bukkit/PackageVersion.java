eaa1909f135bbe90e1db77deef0519223e575653
Logan Barrett <loganb1max@users.noreply.github.com>
2020-08-22 03:34:41 -0500

    Add v1_16_R2 (#3)

--- src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
+++ src/main/java/me/lucko/shadow/bukkit/PackageVersion.java
@@ -56,7 +56,8 @@ public enum PackageVersion {
     v1_13_R2,
     v1_14_R1,
     v1_15_R1,
-    v1_16_R1;
+    v1_16_R1,
+    v1_16_R2;
 
     /**
      * The nms prefix (without the version component)
